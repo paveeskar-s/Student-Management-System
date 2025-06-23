57957851f1ed01495b70799eb465ecd11200a48b
SIVAKARAN PAVEESKAR <165874919+Paviskar@users.noreply.github.com>
2025-06-23 12:05:38 +0530

    forms create

---
+++ Student Management System/Student Management System/UI/SubjectTeacher/TeacherSubjectForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Management_System.UI.SubjectTeacher
+{
+    public partial class TeacherSubjectForm : Form
+    {
+        public TeacherSubjectForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
